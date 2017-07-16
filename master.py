6d679115301682b65266fd73bbb423fa1f8ee514
tommasoascari <tommasoascari@gmail.com>
2017-07-16 16:04:28 +0200

    nit commit

--- master.py
+++ master.py
@@ -1,8 +1,15 @@
 #usr/bin/python3
 # this is the master program for the comfysetup package
 
-import urllib2
 
+import urllib.request
+import filecmp
+
+UPDATE_LIST = "/tmp/comfysetup_new_list.txt"
+
+def compare():
+	compared = filecmp.cmp('list.txt', UPDATE_LIST)
+	return compared
 
 def netcheck():
     try:
@@ -11,11 +18,24 @@ def netcheck():
     except urllib.request.URLError:
         return False
 
-def Update():
-	response = urllib2.urlopen('http://www.example.com/')
-	html = response.read()
-	print(html)
+def updateList():
+	print ("ima working on this")
 
+def update():
+	print (" > Looking for updates ...")
+	URL = "https://raw.githubusercontent.com/tommasoascari/comfysetup/master/list.txt"
+	filename, headers = urllib.request.urlretrieve(url, filename=UPDATE_LIST)
+	check = compare()
+	if check == True:
+		print (' > program mirrorlist is up to date')
+	else if check == False:
+		print (" > Found updates for the program mirrorlist)
+		YN = input(" > Do you want to upgrade the list ? (Y/N) - ")
+		if YN == "y"
+			updateList()
+		else :
+			print ("list not updated")
+			return 0
 
 network = netcheck()
 
